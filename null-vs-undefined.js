e9a62f160e5e49d87b4c49d10d0a50632e60c067
Shabbir0001 <you@example.com>
2021-02-25 19:02:39 +0800

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+let pakhi;
+console.log(pakhi);
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+const result = add(45,23);
+console.log(result);
+
+
+function add2(num1, num2){
+    console.log(num1, num2);
+}
+const result = add2(13);
+console.log(result);
+
+const premik = {name: "Smart Dude", phone: "295427"};
+console.log(premik.gfName);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [54, 33, 34];
+console.log(ages[11]);
+
