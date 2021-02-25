940a9c7d5f6d690a42d92d196968519e9e4ca135
Shabbir0001 <you@example.com>
2021-02-25 18:40:26 +0800

    true vs false

---
+++ true-false.js
@@ -0,0 +1,12 @@
+// const age = 4;
+// const name = " ";
+
+let name = [];
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
