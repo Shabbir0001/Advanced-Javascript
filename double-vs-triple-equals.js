3f197df9df85c45edc1ac5e45907a4730bd470ca
Shabbir0001 <you@example.com>
2021-02-26 13:49:02 +0800

    learnt meny thing

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,13 @@
+const first = 2;
+const second = "2";
+if(first == second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+// double equal checks only the value--
+// triple equal checks the value and the type--
+// 1==true
+// 0==false
---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const numbers = [2, 3, 5, 6, 7, 8, 9];
+// const output = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//    output.push(result); 
+// }
+
+
+// const squere = element => element * element;
+// const squere = x => x*x;
+
+// const result = numbers.map(function(element, index, array){
+//     return element * element;
+// })
+
+
+// const result = numbers.map(x => x*x);
+// console.log(result);
+
+
+// console.log(output);
+
+// const bigger = numbers.filter(x => x>5);
+// console.log(bigger);
+
+const isThere = numbers.find(x => x>5);
+console.log(isThere);
